4b519bd5aec12a2432fdf2488dd33887d8bafa2e
Pragati Agarwal <pragatinov@gmail.com>
2026-03-15 16:05:20 +0530

    padding y removed

--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -231,7 +231,7 @@ export default function ProductList() {
           <main className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
             <div className="flex items-baseline justify-between border-b border-gray-200 pt-24 pb-6">
               <h1 className="text-4xl font-bold tracking-tight text-gray-900">
-                New Arrivals
+                All Products
               </h1>
 
               <div className="flex items-center">
@@ -287,9 +287,7 @@ export default function ProductList() {
             </div>
 
             <section aria-labelledby="products-heading" className="pt-6 pb-24">
-              <h2 id="products-heading" className="sr-only">
-                Products
-              </h2>
+              
 
               <div className="grid grid-cols-1 gap-x-8 gap-y-10 lg:grid-cols-4">
                 {/* Filters */}
@@ -371,10 +369,8 @@ export default function ProductList() {
                 {/* Product grid */}
                 <div className="lg:col-span-3">
                   <div className="bg-white">
-                    <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-                      <h2 className="text-2xl font-bold tracking-tight text-gray-900">
-                        Customers also purchased
-                      </h2>
+                    <div className="mx-auto max-w-2xl px-4 py-0 sm:px-6 sm:py-0 lg:max-w-7xl lg:px-8">
+                  
 
                       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                         {products.map((product) => (
