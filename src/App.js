2d3532dda855ca6bff4d30576ffd10a4466f4bea
Pragati Agarwal <pragatinov@gmail.com>
2026-03-15 12:56:16 +0530

    Navbar added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
-import ProductList from './features/product-list/ProductList';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <ProductList />
+      <Home />
     </div>
   );
 }
