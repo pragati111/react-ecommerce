82fb212b20e864064d49ac15ceb0e9f0cf53181f
Pragati Agarwal <pragatinov@gmail.com>
2026-03-29 15:31:45 +0530

    made some important changes

--- src/features/product/components/ProductList.js
+++ src/features/product/components/ProductList.js
@@ -1,4 +1,5 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
+import { useSelector , useDispatch} from "react-redux";
 import {
   Dialog,
   DialogBackdrop,
@@ -20,8 +21,9 @@ import {
   Squares2X2Icon,
 } from "@heroicons/react/20/solid";
 
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
+import { ChevronLeftIcon, ChevronRightIcon, StarIcon } from '@heroicons/react/20/solid'
 import { Link } from "react-router-dom";
+import { fetchAllProductsAsync,selectAllProducts } from "../productSlice";
 
 const items = [
   { id: 1, title: 'Back End Developer', department: 'Engineering', type: 'Full-time', location: 'Remote' },
@@ -53,11 +55,12 @@ const filters = [
     id: "category",
     name: "Category",
     options: [
-      { value: "new-arrivals", label: "New Arrivals", checked: false },
-      { value: "sale", label: "Sale", checked: false },
-      { value: "travel", label: "Travel", checked: true },
-      { value: "organization", label: "Organization", checked: false },
-      { value: "accessories", label: "Accessories", checked: false },
+      { value: "smartphones", label: "Smartphones", checked: false },
+      { value: "laptops", label: "Laptops", checked: false },
+      { value: "fragrances", label: "Fragrances", checked: true },
+      { value: "skincare", label: "Skincare", checked: false },
+      { value: "groceries", label: "Groceries", checked: false },
+      { value: "home-decoration", label: "Home Decoration", checked: false },
     ],
   },
   {
@@ -78,52 +81,17 @@ function classNames(...classes) {
   return classes.filter(Boolean).join(" ");
 }
 
-const products = [
-  {
-    id: 1,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-01.jpg",
-    imageAlt: "Front of men's Basic Tee in black.",
-    price: "$35",
-    color: "Black",
-  },
-  {
-    id: 2,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-02.jpg",
-    imageAlt: "Front of men's Basic Tee in white.",
-    price: "$35",
-    color: "Aspen White",
-  },
-  {
-    id: 3,
-    name: "Basic Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-03.jpg",
-    imageAlt: "Front of men's Basic Tee in dark gray.",
-    price: "$35",
-    color: "Charcoal",
-  },
-  {
-    id: 4,
-    name: "Artwork Tee",
-    href: "#",
-    imageSrc:
-      "https://tailwindcss.com/plus-assets/img/ecommerce-images/product-page-01-related-product-04.jpg",
-    imageAlt:
-      "Front of men's Artwork Tee in peach with white and brown dots forming an isometric cube.",
-    price: "$35",
-    color: "Iso Dots",
-  },
-];
+
 
 export default function ProductList() {
+  const dispatch = useDispatch();
   const [mobileFiltersOpen, setMobileFiltersOpen] = useState(false);
+  const products = useSelector(selectAllProducts);
+  
+
+  useEffect(()=>{
+    dispatch(fetchAllProductsAsync())
+  }, [dispatch])
 
   return (
     <div>
@@ -377,30 +345,36 @@ export default function ProductList() {
                       <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
                         {products.map((product) => (
                           <Link to='product-detail'>
-                          <div key={product.id} className="group relative">
+                          <div key={product.id} className="group relative border-solid border-2 p-2 border-gray-200">
                             <img
-                              alt={product.imageAlt}
-                              src={product.imageSrc}
-                              className="aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-80"
+                              alt={product.title}
+                              src={product.thumbnail}
+                              className="min-h-40 aspect-square w-full rounded-md bg-gray-200 object-cover group-hover:opacity-75 lg:aspect-auto lg:h-40"
                             />
                             <div className="mt-4 flex justify-between">
                               <div>
                                 <h3 className="text-sm text-gray-700">
-                                  <a href={product.href}>
+                                  <a href={product.thumbnail}>
                                     <span
                                       aria-hidden="true"
                                       className="absolute inset-0"
                                     />
-                                    {product.name}
+                                    {product.title}
                                   </a>
                                 </h3>
                                 <p className="mt-1 text-sm text-gray-500">
-                                  {product.color}
+                                  <StarIcon className="w-5 h-5 inline"/>
+                                  <span className="align-bottom">{product.rating}</span>
                                 </p>
                               </div>
-                              <p className="text-sm font-medium text-gray-900">
-                                {product.price}
+                              <div>
+                                <p className="text-sm font-medium text-gray-900">
+                                ${product.price}
                               </p>
+                              <p className="text-sm font-medium line-through text-gray-400">
+                                ${Math.round(product.price*(1-product.discountPercentage/100))}
+                              </p>
+                              </div>
                             </div>
                           </div>
                           </Link>
